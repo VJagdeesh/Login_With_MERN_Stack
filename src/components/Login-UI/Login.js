1c7adb517410059888e99ef271186d7b2436dc8f
unknown <jagdeeshbhavani22@gmail.com>
2023-08-07 00:25:21 +0530

    Second

--- src/components/Login-UI/Login.js
+++ src/components/Login-UI/Login.js
@@ -1,15 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 
 export default function Login() {
+  const [email, setEmail] = useState();
+  const [password, setPassword] = useState();
+  const submitHandler = (e) => {
+    e.preventDefault();
+    console.log(email, "    ", password);
+  };
   return (
     <div>
       Login
-      <form>
+      <form onSubmit={submitHandler}>
         <label htmlFor="email">E-Mail</label>
-        <input type="text" id="email" />
+        <input
+          type="text"
+          id="email"
+          onChange={(e) => setEmail(e.target.value)}
+        />
         <br></br>
         <label htmlFor="password">Password</label>
-        <input type="password" id="password" />
+        <input
+          type="password"
+          id="password"
+          onChange={(e) => setPassword(e.target.value)}
+        />
         <br></br>
         <button>Submit</button>
       </form>
--- src/components/Register-UI/Register.js
+++ src/components/Register-UI/Register.js
@@ -1,5 +1,21 @@
 import React from "react";
 
 export default function Register() {
-  return <div>Register</div>;
+  return (
+    <div>
+      Register
+      <form>
+        <label htmlFor="email">E-Mail</label>
+        <input type="text" id="email" />
+        <br></br>
+        <label htmlFor="password">Password</label>
+        <input type="password" id="password" />
+        <br></br>
+        <label htmlFor="password">Retype Password</label>
+        <input type="password" id="password" />
+        <br></br>
+        <button>Submit</button>
+      </form>
+    </div>
+  );
 }
